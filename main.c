82da7b144d18a26d46e201d42ff15d1664227eb5
vtrBauer <vitormbl@hotmail.com>
2021-06-07 20:26:13 -0300

    More stuff...

--- main.c
+++ main.c
@@ -95,58 +95,96 @@ typedef struct Buffer{
 
 } Buffer;
 
-Buffer create_buffer(void){
-    Buffer buffer;
-    for(int i = 0; i < BUFFER_SIZE; i++) buffer.stream[i] = '\0';
+
+Buffer* create_buffer(void){
+    Buffer* buffer = malloc(sizeof(Buffer));
+    for(int i = 0; i < BUFFER_SIZE; i++) buffer->stream[i] = '\0';
     return buffer;
 }
 
-void get_input(Buffer b){
+void get_input(Buffer* b){
     int counter = 0;
     char c;
     while(scanf("%c", &c) != '\n' && counter < BUFFER_SIZE){
-        b.stream[counter] = c;
+        b->stream[counter] = c;
+        counter++;
     }
+    b->size = counter;
     // Treat the 100k possible errors from this input
 
 
-void parse_input(Buffer b){
-    input = b.stream;
-    int v[BUFFER_SIZE] = 0; // the number of words will never be as big as the buffer size
-    int word_count = 0;
-
-    for(int i = 0; i < BUFFER_SIZE; i++){// Treat the cases with more than one space in between the words
-        if (input[i] == ' ' || input[i] == '\0'){
-            v[word_count] == i;
-            word_count++;
-            if (input[i] == '\0') break;
+char** parse_input(Buffer* b){
+    int v[b->size * 2] = 0;
+    int v_ind = 0;
+    int i = 0;
+    int mode = 0;
+    while(1){
+        /*
+        mode 0 == recebendo nome
+        mode 1 == espaço em branco
+        */
+        // '\n' after backspaces
+        if (b->stream[i] == '\n' && mode == 1) break;
+        // '\n' after text
+        else if (b->stream[i] == '\n' && mode == 0){
+            v[v_ind] == i - 1;
+            i++;
+            break;
         }
-
-    } 
-
-    char** args = malloc(word_count * sizeof(char*));
-    for(int i = 0; i < word_count; i++){
-        if (i == 0){
-            args[i] = malloc((v[1] - 1) * sizeof(char));
-            char* word[v[1]];
- 
-            for(int k = 0; k < v[1] - 1; k++) word[k] = input[k];
-            word[v[1]-1] = NULL;
-            //strcpy(args[i], )
+        // backspace after backspace
+        else if (b->stream[i] == ' ' && mode == 1) i++;
+        // backspace after text
+        else if (b->stream[i] == ' ' && mode == 0){
+            v[v_ind++] == i - 1;
+            mode = 1;
+            i++;
+        }  
+        // text after backspace
+        else if (b->stream[i] != ' ' && mode == 1){
+            v[v_ind++] = i;
+            mode = 0;
+            i++;
         }
-        args[i] = malloc((v[i] - v[i-1] - 1) * sizeof(char)) // There is no -1 anymore for the '\0' space
+        // text after text
+        else if (b->stream[i] != ' ' && mode == 0)i++;
+        else{
+            i++;
+            // TODO: Treat the unexpected state with the error library
+        }
     }
+
+    // Try to rewrite this whole thing with strcpy()
+    // Issue is, the args in the b->stream do not end with a '\0'.
+    char** args = malloc(b->size * sizeof(char*));
+    for (int j = 0; j < size; j++){
+        arg_size = v[j*2+1] - v[j*2] + 1 + 1; // An additional +1 for the '\0' char. Explicit on purpose.
+        args[j] = malloc(arg_size * sizeof(char));
+        int tmp_c = v[j*2];
+        while(tmp_c <= v[j*2]+1){
+            args[j][tmp_c - v[j*2]] = b->stream[tmp_c];
+            tmp_c++;
+        }
+        args[j][tmp_c] == '\0';
+    }
+
+    // At this stage, args should carry the words followed by '\0'
+
+    return args;
 }
 
-uint8_t create_table(void){
+
+
+Table* create_table(void){
+
+
     
 }
 
-uint8_t create_attribute(){
+Attribute* create_attribute(){
 
 }
 
-uint8_t create_tuple(){
+Tuple* create_tuple(){
 
 }
 
