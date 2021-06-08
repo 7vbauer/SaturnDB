bb2e480b886ef2344407a6059c5dbf4c11ee2a21
vtrBauer <vitormbl@hotmail.com>
2021-06-08 00:43:44 -0300

    More stuff...

--- main.c
+++ main.c
@@ -78,17 +78,7 @@ CHAR <X>
 
 #define BUFFER_SIZE = 256
 
-typedef struct Table{
 
-} TABLE;
-
-typedef struct Attribute{
-    
-} Attribute;
-
-typedef struct Tuple{
-    
-} Tuple;
 
 typedef struct Buffer{
     char stream[BUFFER_SIZE];
@@ -169,22 +159,100 @@ char** parse_input(Buffer* b){
 
     // At this stage, args should carry the words followed by '\0'
 
-    return args;
+    return &args;
 }
 
+typedef struct TABLE_HEADER{
+    uint64_t header_bytes; // how many bytes until data begins
+    uint64_t table_bytes; // how many bytes until whole table data ends == file size
+    char name[256];
+    uint8_t attribute_number; // how many attributes are in the table, max 255 for now
+    ATTRIBUTE_HEADER* attribute_header;
+} TABLE_HEADER;
+
+typedef struct ATTRIBUTE_HEADER{
+    char name[256];
+    char data_type[256];
+    uint64_t data_bytes;
+} ATTRIBUTE_HEADER;
+
+typedef struct DATA{
+    char x;
+} DATA;
+
+typedef struct Table{
+    TABLE_HEADER* table_header;
+    DATA* data;
+} TABLE;
+
+typedef struct Attribute{
+    
+} Attribute;
+
+typedef struct Tuple{
+    
+} Tuple;
+
+typedef struct Table_creation{
+    char name[256];
+    Attribute* 
+} Table_creation;
+/*
+typedef struct DATA_HEADER{
+
+} DATA_HEADER;
+*/
+
+Table* create_table(Table_creation* tb){
+
+    TABLE_HEADER* new_table_header = malloc(sizeof(TABLE_HEADER));
+    new_table_header->name = tb->table_name;
+
+    ATTRIBUTE_HEADER* new_attribute_header = malloc(new_table_header->attribute_number * sizeof(ATTRIBUTE_HEADER));
+
+    if (new_attribute_header == NULL){
+        // TODO: Treat the errors with the error library !!!!!
+        return NULL;
+    }
+
+    new_table_header->attribute_number = tb->attribute_number;
+    new_table_header->attribute_header = new_attribute_header;
+    
+    for(int i = 0; i < new_table_header->attribute_number; i++){
+        new_table_header->attribute_header[i] = malloc(sizeof(ATTRIBUTE_HEADER));
+        if (new_table_header->attribute_header[i] == NULL){
+            return NULL;
+            //Error
+        }
+        (new_table_header->attribute_header[i])->name = (tb->attr[i])->name;
+        (new_table_header->attribute_header[i])->data_type = (tb->attr[i])->data_type;
+        (new_table_header->attribute_header[i])->data_bytes = (tb->attr[i])->data_bytes;
+    }
+
+    Table* new_table = malloc(sizeof(Table));
 
 
-Table* create_table(void){
-
+    return &new_table;
 
     
-}
-
-Attribute* create_attribute(){
 
 }
 
-Tuple* create_tuple(){
+Tuple* create_tuple(ATTRIBUTE_HEADER* attributes, uint8_t quantity){
+    uint64_t total_bytes = 0;
+    for(int i = 0; i < quantity; i++) total_bytes += attributes[i]->data_bytes;
+    uint8_t* block = malloc(total_bytes);
+
+
+}
+
+uint8_t insert_tuple(uint8_t* data){
+    
+    return 1;
+    
+    return 0;
+
+
 
 }
 
