eadec878ec4b9d26ba28784085983b6737b1316d
7vbauer <41764269+7vbauer@users.noreply.github.com>
2021-07-07 17:42:38 -0300

    Update create.c

--- flow/create.c
+++ flow/create.c
@@ -1,4 +1,3 @@
-
 #ifdef linux
 #include <unistd.h>
 #include <stdlib.h>
@@ -12,41 +11,39 @@
 #include <sql.h>
 #include <sqlext.h>
 
-
+#define TABLE_NAME_SIZE 255
+#define ATTRIBUTE_NAME_SIZE 255
+#define ATTRIBUTE_DATATYPE_SIZE 255
 
 typedef struct TABLE_HEADER{
+    uint8_t magic[6]; // file magic numbers, translate to SATURN in ascii
     uint64_t header_bytes; // how many bytes until data begins
     uint64_t table_bytes; // how many bytes until whole table data ends == file size
-    char name[256];
+    char name[TABLE_NAME_SIZE]; // Table name
+    uint64_t tuples;
+    uint64_t tuple_size; // size of the tuple in bytes
     uint8_t attribute_number; // how many attributes are in the table, max 255 for now
     ATTRIBUTE_HEADER* attribute_header;
 } TABLE_HEADER;
 
 typedef struct ATTRIBUTE_HEADER{
-    char name[256];
-    char data_type[256];
-    uint64_t data_bytes;
+    char name[ATTRIBUTE_NAME_SIZE];
+    char data_type[ATTRIBUTE_DATATYPE_SIZE];
+    uint8_t data_bytes;
 } ATTRIBUTE_HEADER;
 
-typedef struct DATA{
-    char x;
-} DATA;
 
-typedef struct Table{
+typedef struct TABLE{
     TABLE_HEADER* table_header;
-    DATA* data;
+    TUPLE* data;
 } TABLE;
 
-typedef struct Attribute{
-    
-} Attribute;
-
 typedef struct Tuple{
-    
+    uint8_t* byte;    
 } Tuple;
 
 typedef struct Table_creation{
-    char name[256];
+    char name[TABLE_NAME_SIZE];
     Attribute* 
 } Table_creation;
 /*
@@ -55,8 +52,40 @@ typedef struct DATA_HEADER{
 } DATA_HEADER;
 */
 
+//remove table_creation struct and work from SQL_EXPRESSION to Table
 Table* create_table(Table_creation* tb){
 
+    // New table.
+    Table* table = malloc(sizeof(Table));
+
+    // New Table's header.
+    TABLE_HEADER* table_header = malloc(sizeof(TABLE_HEADER));
+
+    // Name of the table.
+    table_header->name = <name>;
+
+    // Array of Attribute headers 
+    ATTRIBUTE_HEADER** attr_header = malloc(<n_of_attributes> * sizeof(ATTRIBUTE_HEADER*));
+
+    // Updates the attribute metadata.
+    table->attribute_header = attr_header;
+    table->attribute_number = <tb_attr_n>;
+
+    // Fills the attribute headers
+    for(int i =0; i < <n_of_attributes>; i++){
+        attr_header[i]->name = <tb_attri_i_name>;
+        attr_header[i]->data_type = <tb_attri_i_type>;
+        attr_header[i]->data_bytes = <tb_attri_i_bytes>;
+    } 
+
+    // Calculates the header size
+    // Table will be the same as the header, because there is no data yet.
+    table->header_bytes = 398 + <n_of_attributes> * sizeof(ATTRIBUTE_HEADER);
+    table->table_bytes = table->header_bytes;
+
+    return table;
+
+    /*
     TABLE_HEADER* new_table_header = malloc(sizeof(TABLE_HEADER));
     new_table_header->name = tb->table_name;
 
@@ -86,6 +115,7 @@ Table* create_table(Table_creation* tb){
 
     return &new_table;
 
+    */
     
 
 }
